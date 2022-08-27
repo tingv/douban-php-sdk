c385a3dfb0c58f153cb99803a1ade9f9100efdbf
ThinkPHP <ThinkPHP@gmail.com>
2022-08-27 23:32:27 +0800

    上个版本注释掉了 _v 参数，开始以为被取消了，实际是只有登录和刷新授权时才有

--- src/Config.php
+++ src/Config.php
@@ -12,7 +12,7 @@ class Config
     protected $udid;
     protected $douban_udid = 'eae10a5fa65e262c13d7ab315614f281651f4b17';    // 此参数来源于豆瓣 iOS 客户端 v7.35.0，但豆瓣电影 iOS 客户端也是这个 ID，所以猜测官方客户端都是这个 ID
     protected $version = '7.35.0';  // 客户端版本号
-    // protected $_v = '14757';  // 暂不清楚具体意义，但不同客户端（指的是豆瓣和豆瓣电影 iOS 客户端）似乎不同。不知道同一个客户端不同版本是否不同（未测试）。当前值来源于豆瓣 iOS 客户端 v6.42.0
+    protected $_v = '29971';  // 暂不清楚具体意义，但不同客户端（指的是豆瓣和豆瓣电影 iOS 客户端）似乎不同。不知道同一个客户端不同版本是否不同（未测试）。当前值来源于豆瓣 iOS 客户端 v7.35.0
     protected $access_token = null;
 
     public function __construct($apikey, $client_id, $client_secret, $device_id, $udid)
